a1e2503518cba33ec225ef023f619197e67b30aa
Denilson Oliveira <denilso-oliveira@pop-os.localdomain>
2024-08-14 15:16:57 -0300

    feat/create connection with database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,22 +1,25 @@
 import sqlite3 from 'sqlite3';
 
 const sqlite = sqlite3.verbose();
-const filePath = './myDatabase.db';
+const filePath = './src/database/myDatabase.db';
 
 const db = new sqlite.Database(filePath, (error) => {
   if (error) {
     console.log(error.message);
+    return;
   }
   db.run(
     `CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    passwordHash VARCHAR(255) NOT NULL,
+    passwordSalt VARCHAR(255) NOT NULL
   )`,
     (error) => {
       if (error) {
         console.log(error.message);
+        return;
       }
       console.log('Table created or existed');
     },
