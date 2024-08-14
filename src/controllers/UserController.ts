e43b3700e5fc4cba1339698405a31f7decd13903
Denilson Oliveira <denilso-oliveira@pop-os.localdomain>
2024-08-14 15:19:10 -0300

    feat/add exceptionof errors

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,27 +1,26 @@
 import { Request, Response } from 'express';
-import { z } from 'zod';
+import { UserStoreSchema } from '../model/UserSchema';
 import UserModel from '../model/UserModel';
-
-const UserStoreSchema = z.object({
-  name: z.string().min(3, { message: 'Mandatory name field' }),
-  email: z.string().email({ message: 'Invalid e-mail format' }),
-  password: z
-    .string()
-    .min(3, { message: 'The password must have at least 3 characters' }),
-});
 
 class UserController {
   store(req: Request, res: Response) {
     const { name, email, password } = UserStoreSchema.parse(req.body);
-
     try {
       UserModel.create(name, email, password, (error) => {
         if (error) {
-          res.json(error.message);
+          res.status(400).json({
+            errors: [error.errno === 19 && 'E-mail already used'],
+            created: false,
+          });
+          return;
         }
+        res.status(201).json({
+          created: true,
+        });
       });
-    } catch (error) {
-      return res.json(error);
+    } catch (errors) {
+      res.status(400).json(errors);
+      return;
     }
   }
 }
